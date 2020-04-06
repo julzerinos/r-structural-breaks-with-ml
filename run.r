f904d6386dd90ad3388ee48974ee28e26448cdb1
julzerinos <julzerinos@gmail.com>
2020-04-06 21:51:17 +0200

    Initial model equation

--- run.r
+++ run.r
@@ -26,3 +26,15 @@ mydata = read.csv("sample_data/Developed_6_Portfolios_ME_INV.csv")  # read csv f
 #   in ith panel, regression parameters changes as
 #   B(i+1) = B(i) + d(i)
 #   at unknown t0 (called change point or break point)
+
+x = rbind(c(1, 2, 3, 4))
+B = cbind(c(4, 3, 2, 1))
+d = cbind(c(1, 1, 1, 1))
+I = 1
+
+
+
+e = .5
+
+y = x %*% (B + d * I) + e
+y
