6a0cc98e284ec1caad852dad18a492feb9091942
Kornelos <kornel1902@gmail.com>
2020-04-08 22:07:02 +0200

    to matrix

--- factors3.r
+++ factors3.r
@@ -1,3 +1,5 @@
 Developed_3_Factors <- read_csv("R/r-structural-breaks-with-ml/sample_data/Developed_3_Factors.csv", 
                                 col_types = cols(t = col_skip()))
 
+# matrix X
+X <- cbind(Developed_3_Factors$`Mkt-RF`,Developed_3_Factors$SMB,Developed_3_Factors$HML,Developed_3_Factors$RF)
