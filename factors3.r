0fa4623858cb41ca439b330435a93bc1d8c1edff
Julian Szachowicz <julzerinos@gmail.com>
2020-04-14 19:19:06 +0200

    learning how to save in Rstudio 101

--- factors3.r
+++ factors3.r
@@ -1,5 +1,16 @@
-Developed_3_Factors <- read_csv("./sample_data/Developed_3_Factors.csv", 
+library(readr)
+
+Developed_3_Factors <- read_csv("sample_data/Developed_3_Factors.csv", 
                                 col_types = cols(t = col_skip()))
+
+
+# Initial break-point
+t0 = '200001'
 
 # matrix X
 X <- cbind(Developed_3_Factors$`Mkt-RF`,Developed_3_Factors$SMB,Developed_3_Factors$HML,Developed_3_Factors$RF)
+
+# matrix X\
+
+
+y =  
