6f23679a57d38cb58d1d2a8750dc23cff55f4f1e
Kornelos <kornel1902@gmail.com>
2020-04-14 19:30:30 +0200

    matrixes X and X_hat

--- factors3.r
+++ factors3.r
@@ -4,13 +4,13 @@ Developed_3_Factors <- read_csv("sample_data/Developed_3_Factors.csv",
                                 col_types = cols(t = col_skip()))
 
 
-# Initial break-point
-t0 = '200001'
+# Initial break-point (row 115 corresponds to 01.2000)
+t0 <- 115
 
-# matrix X
-X <- cbind(Developed_3_Factors$`Mkt-RF`,Developed_3_Factors$SMB,Developed_3_Factors$HML,Developed_3_Factors$RF)
+# extract data to matrix form
+panels <- cbind(Developed_3_Factors$`Mkt-RF`,Developed_3_Factors$SMB,Developed_3_Factors$HML,Developed_3_Factors$RF)
 
-# matrix X\
+# get matrixes X and X_hat
+X <-panels[1:t0,1:4]
+X_t <- panels[t0:dim(panels)[1],1:4]
 
-
-y =  
