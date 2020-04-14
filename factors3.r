9b802b32038fe0258b9f0ca66e9c474075791b79
Julian Szachowicz <julzerinos@gmail.com>
2020-04-14 19:15:43 +0200

    introduce initial breakpoint

--- factors3.r
+++ factors3.r
@@ -1,4 +1,4 @@
-Developed_3_Factors <- read_csv("R/r-structural-breaks-with-ml/sample_data/Developed_3_Factors.csv", 
+Developed_3_Factors <- read_csv("./sample_data/Developed_3_Factors.csv", 
                                 col_types = cols(t = col_skip()))
 
 # matrix X
