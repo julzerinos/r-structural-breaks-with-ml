ad5ae5fba3cd895aab02aa49a2a4e688e81bfab1
Julian Szachowicz <julzerinos@gmail.com>
2020-04-15 21:31:57 +0200

    burh moment added random B

--- factors3.r
+++ factors3.r
@@ -10,15 +10,16 @@ t0 <- 115
 panels <- cbind(Developed_3_Factors$`Mkt-RF`,Developed_3_Factors$SMB,Developed_3_Factors$HML,Developed_3_Factors$RF)
 
 # OLS Estimator
-B = solve(t(panels) %*% panels) %*% t(panels) # multiply by Y matrix
+B = do.call("rbind", lapply(1:4, function(x) sample(c(1, rep(0, 3)), 1))) # solve(t(panels) %*% panels) %*% t(panels)
 
 # initial break point estimator
 Y = panels %*% B
+Y
 
 # get matrixes X and X_hat
 X <-panels[1:t0,1:4]
 X_t <- panels[t0:dim(panels)[1],1:4]
 
 # Recalculate OLS Estimators
-B_hat = solve(t(X) %*% X) %*% t(X) %*% Y[1:t0,1:4]
-B_tilde = solve(t(X_t) %*% X_t) %*% t(X_t) %*% Y[t0:dim(panels)[1],1:4]
+B_hat = solve(t(X) %*% X) %*% t(X) %*% Y[1:t0,1]
+B_tilde = solve(t(X_t) %*% X_t) %*% t(X_t) %*% Y[t0:dim(panels)[1],1]
