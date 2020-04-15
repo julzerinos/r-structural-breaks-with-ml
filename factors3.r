e9b10f46ae3bf0e0b0142b05b349a9f90fbdf9b1
Julian Szachowicz <julzerinos@gmail.com>
2020-04-15 21:13:36 +0200

    add OLS estimators

--- factors3.r
+++ factors3.r
@@ -9,13 +9,16 @@ t0 <- 115
 # extract data to matrix form
 panels <- cbind(Developed_3_Factors$`Mkt-RF`,Developed_3_Factors$SMB,Developed_3_Factors$HML,Developed_3_Factors$RF)
 
+# OLS Estimator
+B = solve(t(panels) %*% panels) %*% t(panels) # multiply by Y matrix
+
+# initial break point estimator
+Y = panels %*% B
+
 # get matrixes X and X_hat
 X <-panels[1:t0,1:4]
 X_t <- panels[t0:dim(panels)[1],1:4]
 
-# OLS Estimator
-B = solve(t(X) %*% X) %*% t(X) #
-
-
-# break point estimator
-y = X[1,] %*% B
+# Recalculate OLS Estimators
+B_hat = solve(t(X) %*% X) %*% t(X) %*% Y[1:t0,1:4]
+B_tilde = solve(t(X_t) %*% X_t) %*% t(X_t) %*% Y[t0:dim(panels)[1],1:4]
