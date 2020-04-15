43a5a34f52db41b13291e76f4f5d0547c9e3aba7
Kornelos <kornel1902@gmail.com>
2020-04-15 23:03:57 +0200

    Added test/train split

--- factors3.r
+++ factors3.r
@@ -3,11 +3,20 @@ library(readr)
 Developed_3_Factors <- read_csv("sample_data/Developed_3_Factors.csv", 
                                 col_types = cols(t = col_skip()))
 
-# Initial break-point (row 115 corresponds to 01.2000)
-t0 <- 115
+tresh <- floor(dim(Developed_3_Factors)[1] * 0.75)
+  
+train <- Developed_3_Factors[1:tresh,1:4]
+test <- Developed_3_Factors[tresh:dim(Developed_3_Factors)[1],1:4]
+
+
+
+
+# some initial t0 in train
+t0 <- dim(train)[1]/2+10
 
 # extract data to matrix form
-panels <- cbind(Developed_3_Factors$`Mkt-RF`,Developed_3_Factors$SMB,Developed_3_Factors$HML,Developed_3_Factors$RF)
+# panels <- cbind(Developed_3_Factors$`Mkt-RF`,Developed_3_Factors$SMB,Developed_3_Factors$HML,Developed_3_Factors$RF)
+panels <- cbind(train$`Mkt-RF`,train$SMB,train$HML,train$RF)
 
 # OLS Estimator
 B = do.call("rbind", lapply(1:4, function(x) sample(c(1, rep(0, 3)), 1))) # solve(t(panels) %*% panels) %*% t(panels)
