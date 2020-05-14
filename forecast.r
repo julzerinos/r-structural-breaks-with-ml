a6115643f7e3c44ae223bddbcb90eb76dd8f5ba7
Kornelos <kornel1902@gmail.com>
2020-05-14 15:32:36 +0200

    updated breakpoint finding

--- forecast.r
+++ forecast.r
@@ -5,31 +5,85 @@ library(forecast)
 library(ggplot2)
 
 library(readxl)
-pwt91 <- read_excel("sample_data/pwt91.xlsx")
+pwt91 <- read_excel("sample_data/pwt91.xlsx", sheet = "Data")
 
-greece <- subset(pwt91,country=="Ukraine",select = c("year","rgdpe"))
+greece <- subset(pwt91,country=="Egypt",select = c("year","rgdpe"))
 # remove rows with NA
 greece <- greece[complete.cases(greece),]
 
 # plot gdp 
 # ggplot(greece, aes(x = year, y = rgdpe)) + geom_line()
 
+#start_year <-greece$year[1]
+greece <- ts(greece$rgdpe,start = greece$year[1], frequency=1)
+#greece <- Nile
+start_year = time(greece)[1]
 
-greece <- greece[complete.cases(greece),]
-greece <- as.ts(greece$rgdpe)
 
-
-
-
-plot(greece_ts)
+plot(greece)
+# cusum and mosum for residuals (value - mean) 
 #plot cumsum
-CUSUM = cumsum(greece_ts - mean(greece_ts))
-plot(CUSUM)
+CUSUM = cumsum(greece - mean(greece))
+CUSUM <- ts(CUSUM,start =start_year)
+plot(CUSUM, type ="l")
+abline(a=0,b=0, col="green")
 
-
+  
 # plot moving sum
-MOSUM = rollapply(greece_ts - mean(greece_ts), 15, sum)
+MOSUM = rollapply(greece - mean(greece), 15, sum)
 plot(MOSUM)
+abline(a=0,b=0, col="green")
+
+# plot rec-cumsum (mean of all observations prior to given time)
+rec_cusum <- numeric(length(greece))
+for (i in 1:length(greece)){
+  rec_cusum[i] = mean(greece[1:(i-1)])
+}
+rec_cusum <- ts(rec_cusum,start = start_year,frequency = 1)
+
+plot(greece)
+lines(rec_cusum,col="red")
+
+# plot cumulated recursive residuals
+model <- lm(greece ~ 1)
+plot(greece)
+abline(model)
+# one step ahead prediction errors
+rec_residuals <- numeric(length(greece))
+for (i in 2:length(greece)){
+  rec_residuals[i-1] = predict.lm(model,greece[i])
+}
+  # calculate residual sum of squares
+  
+  rss =  sapply(5:(length(greece)-5), function(i) {
+     before <- 1:i
+     after <- (i+1):length(greece)
+    res <- c(Nile[before] - mean(Nile[before]), Nile[after] - mean(Nile[after]))
+     sum(res^2)
+     })
+
+  plot(start_year + 5:(length(greece)-5), rss, type = "b", xlab = "Time", ylab = "RSS")
+  
+  rss <- ts(rss,start=start_year-1+5,frequency=1)
+  #breakpoint <- min(rss)
+  breakpoint <- time(rss)[which.min(rss)]
+  # local minimma
+ # which(diff(sign(diff(rss)))==+2)+1
+  # combine timeseries with lag
+  
+  
+  
+  plot(greece)
+  abline(v=breakpoint, col="red",lty=2)
+  lines(ts(predict(lm((greece[1:(breakpoint-start_year+1)]) ~ 1)),start=start_year,freq=1),col='darkgreen',lwd=2)
+  lines(ts(predict(lm((greece[(breakpoint-start_year+1):length(greece)]) ~ 1)),start=breakpoint,freq=1),col='darkgreen',lwd=2)
+
+
+
+
+
+
+
 
 #https://robjhyndman.com/hyndsight/piecewise-linear-trends/
 x1 <- 1:length(y)
