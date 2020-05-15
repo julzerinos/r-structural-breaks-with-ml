7830639d24f8eb5b4a2417d6bc53a88c0c0749ce
Kornelos <kornel1902@gmail.com>
2020-05-15 23:16:53 +0200

    Cleanup

--- forecast.r
+++ forecast.r
@@ -1,117 +1,115 @@
   library("zoo")
-  # install.packages('forecast', dependencies = TRUE)
-  library(forecast)
   library(ggplot2)
-  library("bfast")
+  #library("bfast")
   library(readxl)
   library("strucchange")
+  
+  # Read and transfrom data
   pwt91 <- read_excel("sample_data/pwt91.xlsx", sheet = "Data")
-  
-    greece <- subset(pwt91,countrycode=="SYR",select = c("year","rgdpe"))
+  country_gdp <- subset(pwt91,countrycode=="SWZ",select = c("year","rgdpe"))
   # remove rows with NA
-  greece <- greece[complete.cases(greece),]
+  country_gdp <- country_gdp[complete.cases(country_gdp),]
+  country_gdp <- ts(country_gdp$rgdpe,start = country_gdp$year[1], frequency=1)
+  start_year = time(country_gdp)[1]
   
-  # plot gdp 
-  # ggplot(greece, aes(x = year, y = rgdpe)) + geom_line()
-  
-  #start_year <-greece$year[1]
-  greece <- ts(greece$rgdpe,start = greece$year[1], frequency=1)
-  # greece <- Nile  
-  start_year = time(greece)[1]
   
   # calculating percentage growth in time
-  prc <- sapply(1:length(greece)-1, function(i){
-    1-greece[i]/greece[i+1]
+  prc <- sapply(1:length(country_gdp)-1, function(i){
+    1-country_gdp[i]/country_gdp[i+1]
   })
   start_year = start_year + 1
-  greece <- ts(unlist(prc, use.names=FALSE),start = start_year,frequency=1)
+  country_gdp <- ts(unlist(prc, use.names=FALSE),start = start_year,frequency=1)
+
   
-  plot(greece)
+  # Plot the data and CUSUM & MOSUM 
+  plot(country_gdp)
   # cusum and mosum for residuals (value - mean) 
   #plot cumsum
-  CUSUM = cumsum(greece - mean(greece))
+  CUSUM = cumsum(country_gdp - mean(country_gdp))
   CUSUM <- ts(CUSUM,start =start_year)
   plot(CUSUM, type ="l")
   abline(a=0,b=0, col="green")
-  
-    
   # plot moving sum
-  MOSUM = rollapply(greece - mean(greece), 15, sum)
+  MOSUM = rollapply(country_gdp - mean(country_gdp), 15, sum)
   plot(MOSUM)
   abline(a=0,b=0, col="green")
   
   # plot rec-cumsum (mean of all observations prior to given time)
-  rec_cusum <- numeric(length(greece))
-  for (i in 1:length(greece)){
-    rec_cusum[i] = mean(greece[1:(i-1)])
+  rec_cusum <- numeric(length(country_gdp))
+  for (i in 1:length(country_gdp)){
+    rec_cusum[i] = mean(country_gdp[1:(i-1)])
   }
   rec_cusum <- ts(rec_cusum,start = start_year,frequency = 1)
   
-  plot(greece)
+  plot(country_gdp)
   lines(rec_cusum,col="red")
   
   # plot cumulated recursive residuals
-  model <- lm(greece ~ 1)
-  plot(greece)
+  model <- lm(country_gdp ~ 1)
+  plot(country_gdp)
   abline(model)
-  # one step ahead prediction errors
-  
-  #rec_residuals <- numeric(length(greece))
-  #for (i in 2:length(greece)){
-  #  rec_residuals[i-1] = predict.lm(model,greece[i])
-  #}
-    # calculate residual sum of squares
+
+  # calculate residual sum of squares
     
-    rss =  sapply(5:(length(greece)-5), function(i) {
-       before <- 1:i
-       after <- (i+1):length(greece)
-      res <- c(Nile[before] - mean(Nile[before]), Nile[after] - mean(Nile[after]))
-       sum(res^2)
-       })
+  rss =  sapply(5:(length(country_gdp)-5), function(i) {
+    before <- 1:i
+    after <- (i+1):length(country_gdp)
+    res <- c(Nile[before] - mean(Nile[before]), Nile[after] - mean(Nile[after]))
+    sum(res^2)
+    })
   
-    plot(start_year + 5:(length(greece)-5), rss, type = "b", xlab = "Time", ylab = "RSS")
+  plot(start_year + 5:(length(country_gdp)-5), rss, type = "b", xlab = "Time", ylab = "RSS")
     
-    rss <- ts(rss,start=start_year-1+5,frequency=1)
-    #breakpoint <- min(rss)
-    breakpoint <- time(rss)[which.min(rss)]
+  rss <- ts(rss,start=start_year-1+5,frequency=1)
+  breakpoint <- time(rss)[which.min(rss)]
    
     
     
-    plot(greece)
-    abline(v=breakpoint, col="red",lty=2)
-    lines(ts(predict(lm((greece[1:(breakpoint-start_year+1)]) ~ 1)),start=start_year,freq=1),col='darkgreen',lwd=2)
-    lines(ts(predict(lm((greece[(breakpoint-start_year+1):length(greece)]) ~ 1)),start=breakpoint,freq=1),col='darkgreen',lwd=2)
+  plot(country_gdp)
+  abline(v=breakpoint, col="red",lty=2)
+  lines(ts(predict(lm((country_gdp[1:(breakpoint-start_year+1)]) ~ 1)),start=start_year,freq=1),col='darkgreen',lwd=2)
+  lines(ts(predict(lm((country_gdp[(breakpoint-start_year+1):length(country_gdp)]) ~ 1)),start=breakpoint,freq=1),col='darkgreen',lwd=2)
   
   
-  # stuff from paper
+  # Strucchange based calculations
     
-       bp.nile <- breakpoints(greece ~ 1)
-       nile.fac <- breakfactor(bp.nile, breaks = length(bp.nile$breakpoints) )
-       fm1.nile <- lm(greece ~ nile.fac - 1)
-       plot(bp.nile)
-       
-       plot(greece, ylab="RGDPE")
-      # abline(h= mean(greece),col='blue')
-        abline(v= breakpoint-1, lty=2, col='red')
-       lines(ts(predict(fm1.nile),start=start_year,freq=1), col='darkgreen',lwd=2)
-       ci_ts <- confint(bp.nile)
-       lines(ci_ts)
+  bp.nile <- breakpoints(country_gdp ~ 1)
+  nile.fac <- breakfactor(bp.nile, breaks = length(bp.nile$breakpoints) )
+  fm1.nile <- lm(country_gdp ~ nile.fac - 1)
+  plot(bp.nile)
   
-         ## test the null hypothesis that the GDP growth remains constant
-         ## over the years
-         ## compute OLS-based CUSUM process and plot
-         ## with standard and alternative boundaries
-        #TODO: 4 plots combined with all efp tests  
-       ocus <- efp(greece ~ 1, type = "OLS-CUSUM")
-         
-         plot(ocus)
-         abline(v= breakpoint-1, lty=2, col='red')
-         plot(ocus, alpha = 0.01, alt.boundary = TRUE)
-         ## calculate corresponding test statistic
-         sctest(ocus)
-          
-         ## F statistics at 5% significance level
-         fs <- Fstats(greece ~ 1)
-         plot(fs)
+  plot(country_gdp, ylab="RGDPE")
+  # abline(h= mean(country_gdp),col='blue')
+  abline(v= breakpoint-1, lty=2, col='red')
+  lines(ts(predict(fm1.nile),start=start_year,freq=1), col='darkgreen',lwd=2)
+  ci_ts <- confint(bp.nile)
+  lines(ci_ts)
+  
+  ## test the null hypothesis that the GDP growth remains constant
+  ## over the years
+  ## compute OLS-based CUSUM process and plot
+  ## with standard and alternative boundaries
+  #TODO: 4 plots combined with all efp tests
+  
+  par(mfrow=c(2,2))
+  ocus <- efp(country_gdp ~ 1, type = "OLS-CUSUM")
+  omos <- efp(country_gdp ~ 1, type = "OLS-MOSUM")
+  rmos <- efp(country_gdp ~ 1, type = "Rec-CUSUM")
+  rcus <- efp(country_gdp ~ 1, type = "Rec-MOSUM")
+  plot(ocus)
+  plot(omos)
+  plot(rmos)
+  plot(rcus)
+  par(mfrow=c(1,1))
+  
+  #abline(v= breakpoint-1, lty=2, col='red')
+  plot(ocus, alpha = 0.01, alt.boundary = TRUE)
+  ## calculate corresponding test statistic
+  sctest(ocus)
     
+  ## F statistics at 5% significance level h=0.15
+  fs <- Fstats(country_gdp ~ 1,from=0.1)
+  plot(fs)
+
+
   
