2a578b01c02e293a049870edfb6b210b91777cbb
Kornelos <kornel1902@gmail.com>
2020-05-17 19:06:58 +0200

    cleanup ggplot and multi variable regression

--- forecast.r
+++ forecast.r
@@ -1,6 +1,5 @@
-  library("zoo")
+  library("zoo") # time series library
   library(ggplot2) # graphing library
-  #library("bfast")
   library(readxl) # reading excel files library
   library("strucchange") # library implementing breakpoint analysis
   library(ggfortify) # extension for ggplot time series graphing
@@ -8,7 +7,7 @@
   
   # Read and transfrom data
   pwt91 <- read_excel("sample_data/pwt91.xlsx", sheet = "Data")
-  country_gdp <- subset(pwt91,countrycode=="SWZ",select = c("year","rgdpe"))
+  country_gdp <- subset(pwt91,countrycode=="RWA",select = c("year","rgdpe"))
   # remove rows with NA
   country_gdp <- country_gdp[complete.cases(country_gdp),]
   country_gdp <- ts(country_gdp$rgdpe,start = country_gdp$year[1], frequency=1)
@@ -29,12 +28,11 @@
   #plot cumsum
   CUSUM = cumsum(country_gdp - mean(country_gdp))
   CUSUM <- ts(CUSUM,start =start_year)
-  plot(CUSUM, type ="l")
-  abline(a=0,b=0, col="green")
+  autoplot(CUSUM)+ geom_hline(yintercept = 0,color="blue")+labs(title="Cumulative Sum", x="year", y="percent in decimal")
+  
   # plot moving sum
   MOSUM = rollapply(country_gdp - mean(country_gdp), 15, sum)
-  plot(MOSUM)
-  abline(a=0,b=0, col="green")
+  autoplot(MOSUM)+ geom_hline(yintercept = 0,color="blue")+labs(title="Moving Sum from 15 observations", x="year", y="percent in decimal")
   
   # plot rec-cumsum (mean of all observations prior to given time)
   rec_cusum <- numeric(length(country_gdp))
@@ -42,56 +40,38 @@
     rec_cusum[i] = mean(country_gdp[1:(i-1)])
   }
   rec_cusum <- ts(rec_cusum,start = start_year,frequency = 1)
-  
-  plot(country_gdp)
-  lines(rec_cusum,col="red")
-  
-  # plot cumulated recursive residuals
-  model <- lm(country_gdp ~ 1)
-  plot(country_gdp)
-  abline(model)
+  autoplot(rec_cusum)+labs(title="Recursive Cumulative Sum", x="year", y="percent in decimal")
+
 
   # calculate residual sum of squares
-    
   rss =  sapply(5:(length(country_gdp)-5), function(i) {
     before <- 1:i
     after <- (i+1):length(country_gdp)
     res <- c(Nile[before] - mean(Nile[before]), Nile[after] - mean(Nile[after]))
     sum(res^2)
     })
-  
-  plot(start_year + 5:(length(country_gdp)-5), rss, type = "b", xlab = "Time", ylab = "RSS")
-    
+
   rss <- ts(rss,start=start_year-1+5,frequency=1)
+  autoplot(rss) + labs(title = "Residual Sum of Squares",x="Year",y="RSS")
+  
+  # find suspected breakpoint
   breakpoint <- time(rss)[which.min(rss)]
-   
-    
-    
   plot(country_gdp)
   abline(v=breakpoint, col="red",lty=2)
   lines(ts(predict(lm((country_gdp[1:(breakpoint-start_year+1)]) ~ 1)),start=start_year,freq=1),col='darkgreen',lwd=2)
   lines(ts(predict(lm((country_gdp[(breakpoint-start_year+1):length(country_gdp)]) ~ 1)),start=breakpoint,freq=1),col='darkgreen',lwd=2)
   
-  
+  #-------------------------------
   # Strucchange based calculations
-    
-  bp.nile <- breakpoints(country_gdp ~ 1)
-  nile.fac <- breakfactor(bp.nile, breaks = length(bp.nile$breakpoints) )
-  fm1.nile <- lm(country_gdp ~ nile.fac - 1)
-  plot(bp.nile)
+  # 1. Simple model country_gdp ~ 1   (y = β0) β0 = mean
   
-  plot(country_gdp, ylab="RGDPE")
-  # abline(h= mean(country_gdp),col='blue')
-  abline(v= breakpoint-1, lty=2, col='red')
-  lines(ts(predict(fm1.nile),start=start_year,freq=1), col='darkgreen',lwd=2)
-  ci_ts <- confint(bp.nile)
-  lines(ci_ts)
+  # F statistics at 5% significance level, h=0.1 (partition size)
+  fs <- Fstats(country_gdp ~ 1,from=0.1)
+  plot(fs,main="F test")
   
-  ## test the null hypothesis that the GDP growth remains constant
-  ## over the years
-  ## compute OLS-based CUSUM process and plot
-  ## with standard and alternative boundaries
-  #TODO: 4 plots combined with all efp tests
+  
+  # test the null hypothesis that the GDP growth remains constant over the years
+  # compute OLS-based CUSUM & MOSUM process and plot
   
   par(mfrow=c(2,2))
   ocus <- efp(country_gdp ~ 1, type = "OLS-CUSUM")
@@ -104,14 +84,39 @@
   plot(rcus)
   par(mfrow=c(1,1))
   
-  #abline(v= breakpoint-1, lty=2, col='red')
-  plot(ocus, alpha = 0.01, alt.boundary = TRUE)
-  ## calculate corresponding test statistic
-  sctest(ocus)
-    
-  ## F statistics at 5% significance level h=0.15
-  fs <- Fstats(country_gdp ~ 1,from=0.1)
+  # finding date of the breakpoint
+  bp <- breakpoints(country_gdp ~ 1)
+  bfac <- breakfactor(bp, breaks = length(bp$breakpoints))
+  fm <- lm(country_gdp ~ bfac - 1)
+  autoplot(bp)
+  
+  plot(country_gdp, ylab="RGDPE")
+  lines(ts(predict(fm),start=start_year,freq=1), col='darkgreen',lwd=2)
+  # compute 95% confidence intervals
+  ci_ts <- confint(bp)
+  lines(ci_ts)
+  
+  # 2. model with two explanatory variables
+
+  gdp_lag <- cbind(country_gdp, lag(country_gdp, k = -1), lag(country_gdp, k = -3))
+  colnames(gdp_lag) <- c("y", "ylag1", "ylag3")
+  
+  gdp_lag <- window(gdp_lag, start = time(gdp_lag)[4], end = time(gdp_lag)[length(gdp_lag)/3-3])
+  model <- lm(y~ylag1 + ylag3,data=gdp_lag)
+  # EFP 
+  re.seat <- efp(y ~ ylag1 + ylag3, data = gdp_lag, type = "RE")
+  plot(re.seat)
+  sctest(re.seat)
+  # F statistics
+  fs <- Fstats(y ~ ylag1 + ylag3, data = gdp_lag, from = 0.1)
   plot(fs)
+  
+  plot(gdp_lag[,"y"],main="Breapoint from F test",ylab="RGDP",xlab="year")
+  abline(v=time(gdp_lag)[fs$breakpoint],col="red",lty=2)
+  
+
+  
+
 
 
   
